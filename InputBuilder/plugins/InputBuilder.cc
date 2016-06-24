8953c768ff0029d5b863d47ab78eb93f1e9ff0b2
nhanvtran <nhan.v.tran@gmail.com>
2016-06-24 14:43:47 +0200

    adding all the info you need!

--- InputBuilder/plugins/InputBuilder.cc
+++ InputBuilder/plugins/InputBuilder.cc
@@ -34,6 +34,8 @@
 #include "DataFormats/L1TrackTrigger/interface/TTTypes.h"     
 #include "DataFormats/EcalDigi/interface/EcalDigiCollections.h"
 #include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
+#include "CondFormats/L1TObjects/interface/L1CaloEcalScale.h"
+#include "CondFormats/L1TObjects/interface/L1CaloHcalScale.h"
 // class declaration
 //
 
@@ -49,6 +51,8 @@ class InputBuilder : public edm::EDAnalyzer {
       const edm::InputTag L1TrackTag_;
       const edm::InputTag EcalTPTag_;
       const edm::InputTag HcalTPTag_;
+      const L1CaloEcalScale* ecalScale_;
+      const L1CaloHcalScale* hcalScale_;
 
    private:
       virtual void beginJob() override;
@@ -79,7 +83,10 @@ InputBuilder::InputBuilder(const edm::ParameterSet& iConfig):
   EcalTPTag_(iConfig.getParameter<edm::InputTag>("EcalTPTag")),
   HcalTPTag_(iConfig.getParameter<edm::InputTag>("HcalTPTag"))
 {
-   //now do what ever initialization is needed
+  //now do what ever initialization is needed
+  // ECAL and HCAL LSB = 0.5
+  ecalScale_ = new L1CaloEcalScale(0.5);
+  hcalScale_ = new L1CaloHcalScale(0.5);
 
 }
 
@@ -101,22 +108,95 @@ InputBuilder::~InputBuilder()
 void
 InputBuilder::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
+  using namespace edm;
+  typedef std::vector<TTTrack<Ref_PixelDigi_> >         vec_track;
 
-  /// TTTrack
-  edm::Handle< std::vector<TTTrack<Ref_PixelDigi_> > > pixelDigiTTTracks;
+  /// ----------------TRACK INFO-------------------
+  /// Stealing Jia Fu's code!
+  /// https://github.com/jiafulow/SLHCL1TrackTriggerSimulations/blob/master/NTupleTools/src/NTupleTTTracks.cc
+  edm::Handle< vec_track > pixelDigiTTTracks;
   iEvent.getByLabel(L1TrackTag_, pixelDigiTTTracks);
-  std::cout << "pixelDigiTTracks size =  " << pixelDigiTTTracks->size() << std::endl;
+  if (pixelDigiTTTracks.isValid()) {
+      
+    edm::LogInfo("NTupleTracks") << "Size: " << pixelDigiTTTracks->size();
 
+    unsigned nPar = 4;
+    unsigned n = 0;
+    for (vec_track::const_iterator it = pixelDigiTTTracks->begin(); it != pixelDigiTTTracks->end(); ++it) {
+
+      const GlobalVector&          momentum = it->getMomentum(nPar);
+      const GlobalPoint&           poca     = it->getPOCA(nPar);  // point of closest approach
+
+      std::cout << "track info = " << momentum.perp() << "," << momentum.eta() << "," << momentum.phi() << "; poca z = " << poca.z() << std::endl;        
+
+      n++;
+      if (n > 10) break; // just for testing so cut it off for now
+    }  
+  }
+
+  /// ----------------ECAL INFO-------------------
+  /// Stealing Jia Fu's code!
+  /// https://github.com/jiafulow/SLHCL1TrackTriggerSimulations/blob/master/NTupleTools/src/NTupleTTTracks.cc
   /// Ecal TPs
   edm::Handle< EcalTrigPrimDigiCollection > ecalTPs;
   iEvent.getByLabel(EcalTPTag_, ecalTPs);
   std::cout << "ecalTPs size =  " << ecalTPs->size() << std::endl;
+  if (ecalTPs.isValid()){
+    unsigned ne = 0;
+    for (EcalTrigPrimDigiCollection::const_iterator it = ecalTPs->begin(); it != ecalTPs->end(); ++it) {
 
+      short ieta = (short) it->id().ieta(); 
+      unsigned short absIeta = (unsigned short) abs(ieta);
+      short sign = ieta/absIeta;
+      
+      // unsigned short cal_iphi = (unsigned short) it->id().iphi(); 
+      // unsigned short iphi = (72 + 18 - cal_iphi) % 72; // transform TOWERS (not regions) into local rct (intuitive) phi bins
+      
+      unsigned short compEt = it->compressedEt();
+      double et = 0.;
+      if (ecalScale_!=0) et = ecalScale_->et( compEt, absIeta, sign );
+
+      if (et > 0) std::cout << "ecal info: " << it->id().ieta() << "," << it->id().iphi() << "," << et << std::endl;
+
+      ne++;
+      // if (ne > 20) break;
+    }
+  }
+
+  /// ----------------HCAL INFO-------------------
+  /// Stealing some other code!
+  /// https://github.com/cms-sw/cmssw/blob/0397259dd747cee94b68928f17976224c037057a/L1Trigger/L1TNtuples/src/L1AnalysisCaloTP.cc#L40
   /// Hcal TPs
   edm::Handle< HcalTrigPrimDigiCollection > hcalTPs;
   iEvent.getByLabel(HcalTPTag_, hcalTPs);
   std::cout << "hcalTPs size =  " << hcalTPs->size() << std::endl;
+  if (hcalTPs.isValid()){
+    
+    unsigned nh = 0;
+
+    for (HcalTrigPrimDigiCollection::const_iterator it = hcalTPs->begin(); it != hcalTPs->end(); ++it) {
+
+      short ieta = (short) it->id().ieta(); 
+      unsigned short absIeta = (unsigned short) abs(ieta);
+      short sign = ieta/absIeta;
+
+      unsigned short cal_iphi = (unsigned short) it->id().iphi();
+      unsigned short iphi = (72 + 18 - cal_iphi) % 72;
+      if (absIeta >= 29) {  // special treatment for HF
+        iphi = iphi/4;
+      }
+
+      unsigned short compEt = it->SOI_compressedEt();
+      double et = 0.;
+      if (hcalScale_!=0) et = hcalScale_->et( compEt, absIeta, sign );
+
+      std::cout << "hcal info: " << it->id().ieta() << "," << it->id().iphi() << "," << et << std::endl;
+
+      nh++;
+      if (nh > 20) break;
+
+    }
+  }
 
 }
 
