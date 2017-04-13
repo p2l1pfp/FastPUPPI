8188f292215c99de23f48e677288505005f8e1b5
nhanvtran <nhan.v.tran@gmail.com>
2017-04-13 18:26:31 +0200

    adding the new muon getting

--- NtupleProducer/python/runNtupleProducer_cfg.py
+++ NtupleProducer/python/runNtupleProducer_cfg.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("OUT")
 process.load('Configuration.StandardSequences.Services_cff')
-process.load('Configuration.Geometry.GeometryExtended2023D3Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D12Reco_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
@@ -17,11 +17,7 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 10
 process.source = cms.Source("PoolSource",
                             # replace 'myfile.root' with the source file you want to use
                             fileNames = cms.untracked.vstring(
-        #'/store/relval/CMSSW_8_1_0_pre15/RelValSingleMuPt100Extended/GEN-SIM-RECO/PU25ns_81X_upgrade2023_realistic_v3_2023D1PU140-v1/10000/204D0758-109A-E611-856C-003048FF9ABC.root'
-        #'/store/relval/CMSSW_8_1_0_pre15/RelValSingleMuPt100Extended/GEN-SIM-RECO/PU25ns_81X_upgrade2023_realistic_v3_2023D3PU140-v1/10000/0824BBCA-119A-E611-B39E-0025905A48F0.root'
-        #'/store/relval/CMSSW_8_1_0_pre15/RelValSingleMuPt100Extended/GEN-SIM-RECO/PU25ns_81X_upgrade2023_realistic_v3_2023D4PU140-v1/10000/025FF191-119A-E611-B262-0025905B8594.root'
-        '/store/relval/CMSSW_8_1_0_pre15/RelValZMM_13/GEN-SIM-RECO/PU25ns_81X_upgrade2023_realistic_v3_2023D3Timing13TeVPU140r1-v1/10000/DEF317C3-28A1-E611-9428-0CC47A4C8E2E.root'
-        #/store/relval/CMSSW_8_1_0_pre15/RelValZMM_13/GEN-SIM-RECO/81X_upgrade2023_realistic_v3_2023D3Timing13TeV-v1/10000/44798C42-6F99-E611-A504-0CC47A7C354C.root'
+        '/store/relval/CMSSW_9_1_0_pre1/RelValZMM_14/GEN-SIM-RECO/90X_upgrade2023_realistic_v9_D12-v1/00000/0A8B6505-A215-E711-B25B-0CC47A4C8F26.root'
         )
 )
 
@@ -36,7 +32,7 @@ process.InfoOut = cms.EDProducer('NtupleProducer',
                                  HGHcalTPTag = cms.InputTag('l1tPFHGCalFHProducerFromOfflineRechits','towers'),
                                  BHHcalTPTag = cms.InputTag('l1tPFHGCalBHProducerFromOfflineRechits','towers'),
                                  HFTPTag     = cms.InputTag('l1tPFHFProducerFromOfflineRechits','towers'),
-                                 MuonTPTag   = cms.InputTag('simGmtDigis'), 
+                                 MuonTPTag   = cms.InputTag('gmtStage2Digis','Muon'), 
                                  genParTag   = cms.InputTag('genParticles'),
                                  zeroSuppress = cms.bool(False),
                                  corrector   = cms.InputTag("/afs/cern.ch/user/p/pharris/pharris/public/bacon/prod/CMSSW_8_1_0_pre16/src/FastPUPPI/NtupleProducer/data/pion_eta_phi.root"),
