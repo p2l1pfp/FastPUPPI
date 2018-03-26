0ab35c2f18fd5c151ac26320cd0094aff4dfdc1b
Giovanni <gpetruc@gmail.com>
2018-03-26 11:30:55 +0200

    try 10X

--- NtupleProducer/python/reprocess_L1Phase2_MC_cff.py
+++ NtupleProducer/python/reprocess_L1Phase2_MC_cff.py
@@ -1,59 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-HcalHardcodeGeometryEP = cms.ESProducer("HcalHardcodeGeometryEP",
-    UseOldLoader = cms.bool(False)
-)
-
-
-HcalTPGCoderULUT = cms.ESProducer("HcalTPGCoderULUT",
-    FGLUTs = cms.FileInPath('CalibCalorimetry/HcalTPGAlgos/data/HBHE_FG_LUT.dat'),
-    LUTGenerationMode = cms.bool(True),
-    MaskBit = cms.int32(32768),
-    RCalibFile = cms.FileInPath('CalibCalorimetry/HcalTPGAlgos/data/RecHit-TPG-calib.dat'),
-    inputLUTs = cms.FileInPath('CalibCalorimetry/HcalTPGAlgos/data/inputLUTcoder_physics.dat'),
-    read_Ascii_LUTs = cms.bool(False),
-    read_FG_LUTs = cms.bool(False),
-    read_XML_LUTs = cms.bool(False)
-)
-
-
-HcalTrigTowerGeometryESProducer = cms.ESProducer("HcalTrigTowerGeometryESProducer")
-
-CaloGeometryBuilder = cms.ESProducer("CaloGeometryBuilder",
-    SelectedCalos = cms.vstring('HCAL', 
-        'ZDC', 
-        'EcalBarrel', 
-        'TOWER', 
-        'HGCalEESensitive', 
-        'HGCalHESiliconSensitive')
-)
-
-
-CaloTPGTranscoder = cms.ESProducer("CaloTPGTranscoderULUTs",
-    HFTPScaleShift = cms.PSet(
-        NCT = cms.int32(1),
-        RCT = cms.int32(3)
-    ),
-    LUTfactor = cms.vint32(1, 2, 5, 0),
-    RCTLSB = cms.double(0.25),
-    ZS = cms.vint32(4, 2, 1, 0),
-    hcalLUT1 = cms.FileInPath('CalibCalorimetry/CaloTPG/data/outputLUTtranscoder_physics.dat'),
-    hcalLUT2 = cms.FileInPath('CalibCalorimetry/CaloTPG/data/TPGcalcDecompress2.txt'),
-    ietaLowerBound = cms.vint32(1, 18, 27, 29),
-    ietaUpperBound = cms.vint32(17, 26, 28, 32),
-    nominal_gain = cms.double(0.177),
-    read_Ascii_Compression_LUTs = cms.bool(False),
-    read_Ascii_RCT_LUTs = cms.bool(False)
-)
-
-
-CaloTopologyBuilder = cms.ESProducer("CaloTopologyBuilder")
-
-
-CaloTowerHardcodeGeometryEP = cms.ESProducer("CaloTowerHardcodeGeometryEP")
-
-
-CaloTowerTopologyEP = cms.ESProducer("CaloTowerTopologyEP")
+from SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff import *
+from CalibCalorimetry.CaloTPG.CaloTPGTranscoder_cfi import *
 
 from L1Trigger.L1THGCal.hgcalTriggerPrimitives_cff import *
 hgcl1tpg_step = cms.Sequence(hgcalTriggerPrimitives)
@@ -61,8 +9,8 @@ hgcl1tpg_step = cms.Sequence(hgcalTriggerPrimitives)
 from SimCalorimetry.EcalEBTrigPrimProducers.ecalEBTriggerPrimitiveDigis_cff import *
 EcalEBtp_step = cms.Sequence(simEcalEBTriggerPrimitiveDigis)
 
-from SimCalorimetry.HcalTrigPrimProducers.hcalTTPDigis_cff import *
-HcalTPsimulation_step = cms.Sequence(hcalTTPSequence)
+#from SimCalorimetry.HcalTrigPrimProducers.hcalTTPDigis_cff import *
+#HcalTPsimulation_step = cms.Sequence(hcalTTPSequence)
 
 from Configuration.StandardSequences.SimL1Emulator_cff import *
 L1simulation_step = cms.Sequence(SimL1Emulator)
@@ -71,5 +19,9 @@ from L1Trigger.TrackFindingTracklet.L1TrackletTracks_cff import *
 L1TrackTrigger_step = cms.Sequence(L1TrackletTracks)
 
 reprocess_L1Phase2_MC = cms.Sequence(
-    hgcl1tpg_step + EcalEBtp_step + L1TrackTrigger_step + HcalTPsimulation_step
+    L1simulation_step + 
+    hgcl1tpg_step + 
+    #EcalEBtp_step + 
+    L1TrackTrigger_step 
+    #HcalTPsimulation_step
 )
--- NtupleProducer/python/runInputs.py
+++ NtupleProducer/python/runInputs.py
@@ -8,25 +8,22 @@ process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '93X_upgrade2023_realistic_v2', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '100X_upgrade2023_realistic_v1', '')
 
-#process.load('Configuration.StandardSequences.SimL1Emulator_cff')
-#process.load('Configuration.StandardSequences.L1TrackTrigger_cff')
-#process.load('SimCalorimetry.HcalTrigPrimProducers.hcalTTPDigis_cff')
 process.load('FastPUPPI.NtupleProducer.reprocess_L1Phase2_MC_cff')
 
-import EventFilter.EcalRawToDigi.EcalUnpackerData_cfi
-process.ecalDigis = EventFilter.EcalRawToDigi.EcalUnpackerData_cfi.ecalEBunpacker.clone()
-import EventFilter.ESRawToDigi.esRawToDigi_cfi
-process.ecalPreshowerDigis = EventFilter.ESRawToDigi.esRawToDigi_cfi.esRawToDigi.clone()
-import EventFilter.HcalRawToDigi.HcalRawToDigi_cfi
-process.hcalDigis = EventFilter.HcalRawToDigi.HcalRawToDigi_cfi.hcalDigis.clone()
-process.load('RecoLuminosity.LumiProducer.bunchSpacingProducer_cfi')
-process.load('RecoLocalCalo.Configuration.RecoLocalCalo_cff')
+#import EventFilter.EcalRawToDigi.EcalUnpackerData_cfi
+#process.ecalDigis = EventFilter.EcalRawToDigi.EcalUnpackerData_cfi.ecalEBunpacker.clone()
+#import EventFilter.ESRawToDigi.esRawToDigi_cfi
+#process.ecalPreshowerDigis = EventFilter.ESRawToDigi.esRawToDigi_cfi.esRawToDigi.clone()
+#import EventFilter.HcalRawToDigi.HcalRawToDigi_cfi
+#process.hcalDigis = EventFilter.HcalRawToDigi.HcalRawToDigi_cfi.hcalDigis.clone()
+#process.load('RecoLuminosity.LumiProducer.bunchSpacingProducer_cfi')
+#process.load('RecoLocalCalo.Configuration.RecoLocalCalo_cff')
 
 process.load('L1Trigger.L1TTrackMatch.L1TkObjectProducers_cff')
 
-process.load('FastPUPPI.NtupleProducer.l1tPFCaloProducersFromOfflineRechits_cff')
+#process.load('FastPUPPI.NtupleProducer.l1tPFCaloProducersFromOfflineRechits_cff')
 process.load('FastPUPPI.NtupleProducer.l1tPFEcalProducerFromTPDigis_cfi')
 process.load('FastPUPPI.NtupleProducer.l1tPFHcalProducerFromTPDigis_cfi')
 process.load('FastPUPPI.NtupleProducer.l1tPFHGCalProducerFrom3DTPs_cfi')
@@ -36,21 +33,29 @@ process.load('FastPUPPI.NtupleProducer.l1tPFMuProducerFromL1Mu_cfi')
 process.l1tPFMuProducerFromL1Mu.MuonTag = "simGmtStage2Digis"
 process.load('FastPUPPI.NtupleProducer.l1tPFEcalProducerFromL1EGCrystalCluster_cfi')
 
-#process.load('FastPUPPI.NtupleProducer._cfi')
-
 process.source = cms.Source("PoolSource",
-                fileNames = cms.untracked.vstring( 'file:/eos/cms/store/cmst3/user/gpetrucc/l1phase2/93X/TTbar/GEN-SIM-DIGI-RAW/TTbar_job1_sub14.GEN-SIM-DIGI-RAW.root' ),
-                duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+    #fileNames = cms.untracked.vstring('/store/relval/CMSSW_9_3_7/RelValSingleTauFlatPt2To100_pythia8/GEN-SIM-DIGI-RAW/93X_upgrade2023_realistic_v5_2023D17noPU-v2/10000/58739462-8B2C-E811-A013-0CC47A7C34D0.root'),
+    fileNames = cms.untracked.vstring('file:/tmp/gpetrucc/tau.root'),
+    duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+    inputCommands = cms.untracked.vstring("keep *", 
+        "drop l1tHGCalTowerMapBXVector_hgcalTriggerPrimitiveDigiProducer_towerMap_HLT",
+        "drop l1tEMTFHit2016Extras_simEmtfDigis_CSC_HLT",
+        "drop l1tEMTFHit2016Extras_simEmtfDigis_RPC_HLT",
+        "drop l1tEMTFHit2016s_simEmtfDigis__HLT",
+        "drop l1tEMTFTrack2016Extras_simEmtfDigis__HLT",
+        "drop l1tEMTFTrack2016s_simEmtfDigis__HLT")
+
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(50))
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
   
 process.s = cms.Sequence(
     process.reprocess_L1Phase2_MC +
-    process.ecalDigis + process.ecalPreshowerDigis + process.hcalDigis + process.bunchSpacingProducer + process.ecalLocalRecoSequence + process.hcalLocalRecoSequence + process.hcalGlobalRecoSequence + process.hgcalLocalRecoSequence +
-    process.L1TkElectrons + process.L1TkPhotons + process.L1TkJets + process.L1TkPrimaryVertex + process.L1TkEtMiss + process.L1TkHTMissVtx +
-    process.l1tPFEcalProducerFromOfflineRechits + process.l1tPFHcalProducerFromOfflineRechits + process.l1tPFHFProducerFromOfflineRechits + process.l1tPFHGCalEEProducerFromOfflineRechits + process.l1tPFHGCalFHProducerFromOfflineRechits + process.l1tPFHGCalBHProducerFromOfflineRechits +
+    #process.ecalDigis + process.ecalPreshowerDigis + process.hcalDigis + process.bunchSpacingProducer + process.ecalLocalRecoSequence + process.hcalLocalRecoSequence + process.hcalGlobalRecoSequence + process.hgcalLocalRecoSequence +
+    process.L1TkElectrons + process.L1TkPhotons + process.L1TkJets + process.L1TkPrimaryVertex + process.L1TkEtMiss + process.L1TkHTMissVtx +  process.L1TkIsoElectrons + process.L1TkTauFromCalo + 
+    # process.L1TkMuons + ## <<< muons need a fix, see https://hypernews.cern.ch/HyperNews/CMS/get/L1TriggerUpgrades/278/1.html
+    #process.l1tPFEcalProducerFromOfflineRechits + process.l1tPFHcalProducerFromOfflineRechits + process.l1tPFHFProducerFromOfflineRechits + process.l1tPFHGCalEEProducerFromOfflineRechits + process.l1tPFHGCalFHProducerFromOfflineRechits + process.l1tPFHGCalBHProducerFromOfflineRechits +
     process.l1tPFEcalProducerFromTPDigis +
     process.L1EGammaCrystalsProducer + process.l1tPFEcalProducerFromL1EGCrystalClusters +
     process.l1tPFHcalProducerFromTPDigis +
@@ -62,10 +67,10 @@ process.s = cms.Sequence(
 
 process.p = cms.Path(process.s)
 
-process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.out = cms.OutputModule("PoolOutputModule",
-        fileName = cms.untracked.string("inputs_17D.root"),
+        fileName = cms.untracked.string("inputs.root"),
         outputCommands = cms.untracked.vstring("drop *",
             "keep *_genParticles_*_*",
             "keep *_ak4GenJetsNoNu_*_*",
@@ -82,4 +87,5 @@ process.out = cms.OutputModule("PoolOutputModule",
 )
 process.e = cms.EndPath(process.out)
 
+process.schedule = cms.Schedule([process.p,process.e])
 
