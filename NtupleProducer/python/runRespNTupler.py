e2dc9d73efbb0aec56bdaabaa8a4b3c0ce0135da
Giovanni <gpetruc@gmail.com>
2019-03-19 16:53:49 +0100

    runRespNTupler.py and runPerformanceNTuple.py: drop existing PF objects on input, to make sure one is not getting by mistake obsolete stuff and be able to mix different input files produced with different PF versions

--- NtupleProducer/python/runRespNTupler.py
+++ NtupleProducer/python/runRespNTupler.py
@@ -14,7 +14,11 @@ process.source = cms.Source("PoolSource",
     #fileNames = cms.untracked.vstring('file:inputs.root'),
     fileNames = cms.untracked.vstring('file:/eos/cms/store/cmst3/user/gpetrucc/l1tr/105X/NewInputs104X/010319/ParticleGun_PU0/inputs104X_ParticleGun_PU0_job1.root'),
     duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
-    skipBadFiles = cms.untracked.bool(True)
+    skipBadFiles = cms.untracked.bool(True),
+    inputCommands = cms.untracked.vstring("keep *", 
+            "drop l1tPFClusters_*_*_*",
+            "drop l1tPFTracks_*_*_*",
+            "drop l1tPFCandidates_*_*_*")
 )
 
 process.load('Configuration.Geometry.GeometryExtended2023D35Reco_cff')
