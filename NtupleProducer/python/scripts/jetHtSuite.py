da973c41e4d91463d9137fadc90bdfbea22b684b
Giovanni <gpetruc@gmail.com>
2019-03-20 18:08:01 +0100

    Fix ROC plots

--- NtupleProducer/python/scripts/jetHtSuite.py
+++ NtupleProducer/python/scripts/jetHtSuite.py
@@ -2,7 +2,6 @@ import os, re
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 ROOT.gROOT.SetBatch(True)
-ROOT.gROOT.ProcessLine(".x %s/cpp/tdrstyle.cc" % os.environ['HOME']);
 ROOT.gStyle.SetOptStat(False)
 ROOT.gStyle.SetErrorX(0.5)
 ROOT.gErrorIgnoreLevel = ROOT.kWarning
@@ -151,7 +150,7 @@ def makeRecoMETArray(tree, what, obj, etaCut):
 
 def genCutCorrArray(corrArray, genArray, genThr):
     if len(genArray) != len(corrArray): raise RuntimeError("Mismatch")
-    return [ r for (r,g) in zip(genArray,corrArray) if g > genThr ]
+    return [ r for (g,r) in zip(genArray,corrArray) if g > genThr ]
 
 def makeCumulativeHTEff(name, corrArray, xmax, norm=2760.0*11246/1000):
     if len(corrArray) == 0: return None
